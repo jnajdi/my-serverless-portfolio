3fde49db67f329a92b5eb8e0e3d1863afc553336
Jihane Najdi <jihane.najdi@vt.edu>
2018-01-04 14:46:19 -0800

    Create React Component

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work'
 
-console.log("Loaded react-dom");
-console.log("Webpack updated");
+const myWork = [
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving chemistry",
+      'src': "images/example2.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Portfolio Example",
+    'image': {
+      'desc': "example screenshot of a project involving cats",
+      'src': "images/example4.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Work Example Name",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'src': "images/example1.png",
+      'comment': ""
+    }
+  }
+
+]
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
