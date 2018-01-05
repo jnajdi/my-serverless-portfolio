1225867d812513f25ac47865b9620727df225397
Jihane Najdi <jihane.najdi@vt.edu>
2018-01-05 12:11:57 -0800

    Unit testing for React using enzyme and jest

--- __tests__/test-example.js
+++ __tests__/test-example.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { shallow, mount, render }  from 'enzyme';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16'
+import { shallow, mount, render, configure }  from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import ExampleWork , {ExampleWorkBubble} from '../js/example-work';
 
 configure({adapter: new Adapter()});
@@ -32,7 +32,7 @@ describe("ExampleWork Component", () => {
   it("Should be a 'section' element", () => {
     console.log(component.debug());
 
-    expect(component.type()).toEqual('section');
+    expect(component.type()).toEqual('span');
 
 
   });
@@ -40,10 +40,21 @@ describe("ExampleWork Component", () => {
   it("Should contain as many children as there are work examples", () => {
     expect(component.find("ExampleWorkBubble").length).toEqual(myWork.length);
   });
+
+  it("Should allow the modal to open and close", () => {
+    component.instance().openModal();
+    expect(component.instance().state.modalOpen).toBe(true);
+
+    component.instance().closeModal();
+    expect(component.instance().state.modalOpen).toBe(false);
+  })
 });
 
 describe("ExampleWorkBubble component", () => {
-  let component = shallow(<ExampleWorkBubble example={myWork[1]}/>);
+  let mockOpenModalFn = jest.fn();
+
+  let component = shallow(<ExampleWorkBubble example={myWork[1]} openModal = {mockOpenModalFn}/>);
+
   let images = component.find("img");
 
   it("Should contain a single 'img' element", () => {
@@ -53,5 +64,11 @@ describe("ExampleWorkBubble component", () => {
   it("Should have the image src set correctly", () => {
     console.log(images.getElement(0).props);
     expect(images.getElement(0).props.src).toEqual(myWork[1].image.src);
+  });
+
+  it("Should call the openModal handler when clicked", () => {
+    component.find(".section__exampleWrapper").simulate('click');
+    expect(mockOpenModalFn).toHaveBeenCalled();
   })
+
 });
